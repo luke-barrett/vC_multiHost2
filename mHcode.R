0aa8d2951348cb9247c83e575f821a2d9e7f2c3b
Luke Barrett <luke.barrett@csiro.au>
2018-03-22 16:25:46 +1100

    1st commit

---
+++ mHcode.R
@@ -0,0 +1,15 @@
+setwd("~/ARC/ARC work/within-population/2018 analysis/Data/")
+
+library(tidyverse)
+
+multiHostDat<-read_csv("legume_weights_rhizobia_20str.csv")
+
+mHdFac<-multiHostDat %>% 
+  mutate(Strain=as.factor(Strain))
+
+ggplot(data=mHdFac, aes(x=weight))+
+  geom_histogram(binwidth = 0.1)
+
+ggplot(data=mHdFac, aes(x=weight, y=Strain))+
+  geom_point()+
+  facet_grid(~Plant, scales = "free_x")
